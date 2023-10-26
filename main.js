991dac36e54825b13198e0a0c9888f0bcbbf8853
Logan King <logan.ramzie.king@gmail.com>
2023-10-25 19:26:53 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+let variable1 = false;
+console.log(variable1);
+
+
+let variable2 = null;
+console.log(variable2);
+
+let variable3;
+console.log(variable3);
+
+let variable4 = 25;
+console.log(variable4);
+
+let variable5 = 25.2222;
+console.log(variable5);
+
+let variable6 = 25.2e10;
+console.log(variable6);
+
+//start of the req lecture activity
+
+console.log(8 * null);
+
+console.log("5" - 1);
+
+console.log("5" + 1);
+
+console.log("Five" * 2);
+
+console.log(false == 0);
+
+console.log(5 > 2 && 6 > 3);
+
+console.log(false == false ? "equal" : "not equal");
+
+console.log(true || false);
